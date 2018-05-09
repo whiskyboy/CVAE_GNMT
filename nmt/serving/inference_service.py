a3789eb7fe634b466607ca624c5480d8994126b6
tiwe <apiuser@linux-vm.j10eptfpg1tufanyz1gmqiklac.hx.internal.cloudapp.net>
2018-05-09 02:39:51 +0000

    reorganize file structure

--- nmt/serving/inference_service.py
+++ nmt/serving/inference_service.py
@@ -1,6 +1,7 @@
 #coding:utf8
 from __future__ import print_function
 
+import os
 import sys
 import time
 import argparse
@@ -23,13 +24,16 @@ class AlphaCommentServer(object):
         default_hparams = nmt.create_hparams(FLAGS)
         self.hparams = nmt.create_or_load_hparams(model_dir, default_hparams, FLAGS.hparams_path, save_hparams=False)
         self.hparams.beam_width = 0 # force use greedy decoder for inference
+        self.hparams.src_vocab_file = os.path.join(model_dir, "vocab.in")
+        self.hparams.tgt_vocab_file = os.path.join(model_dir, "vocab.out")
         self.ckpt = tf.train.latest_checkpoint(model_dir)
         self.infer_model = model_helper.create_infer_model(cvae_model.CVAEModel, self.hparams)
         self.sess = tf.Session(graph=self.infer_model.graph, config=utils.get_config_proto())
-        self.loaded_infer_model = model_helper.load_model(
+        with self.infer_model.graph.as_default():
+            self.loaded_infer_model = model_helper.load_model(
                 self.infer_model.model, self.ckpt, self.sess, "infer")
 
-    def comment(self, title, sample_num=30, batch_size=32):
+    def comment(self, title, sample_num=50, batch_size=50):
         if batch_size > sample_num:
             batch_size = sample_num
 
@@ -62,15 +66,10 @@ class AlphaCommentServer(object):
                         sent_id,
                         tgt_eos=self.hparams.eos,
                         subword_option=self.hparams.subword_option)
-                    comment_list.append(translation)
+                    if len(translation.split()) == len(set(translation.split())):
+                        comment_list.append(translation)
             except tf.errors.OutOfRangeError:
                 utils.print_time(
                     "  done, num of outputs %d" % num_sentences, start_time)
                 break
         return comment_list
-
-if __name__=="__main__":
-    server = AlphaCommentServer(model_dir=sys.argv[1])
-    title = "朱婷 实现 mvp 俱乐部 大满贯 ， 昔日 对手 ： 唯一 佩服 的 就是 中国 婷 ！"
-    for comment in server.comment(title):
-        print(comment)
