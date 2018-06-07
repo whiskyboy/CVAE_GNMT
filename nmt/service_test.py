0b062591f746b9291776b038dac0472da50b8c1b
tiwe <tiwe@microsoft.com>
2018-06-07 07:31:37 +0000

    update service test code

--- nmt/service_test.py
+++ nmt/service_test.py
@@ -1,15 +1,39 @@
 #coding:utf8
 import sys
-from serving.inference_service import AlphaCommentServer
+from tqdm import tqdm
+import requests
+
+def getGenerativeCommentList(query):
+    host = "http://0.0.0.0:80/AlphaComment"
+    data = {
+        "Title": query.strip(),
+        "SampleNum": 80,
+        "BatchSize": 80,
+        "LMScore": True
+    }
+    response = requests.post(host, json=data)
+    if response:
+        response = response.json()
+        if response.has_key("CommentList"):
+            commentList = []
+            for detail in response["CommentList"]:
+                comment = detail["Comment"]
+                score = detail["Score"]
+                commentList.append((comment.encode("utf8"), float(score)))
+            return commentList
 
 if __name__=="__main__":
-    server = AlphaCommentServer(model_dir=sys.argv[1])
-    title = "朱婷 实现 mvp 俱乐部 大满贯 ， 昔日 对手 ： 唯一 佩服 的 就是 中国 婷 ！"
-    for comment, score in server.comment(title):
-        print("%s\t%s"%(comment, score))
-    title = "总结 ： 长寿 的 人 ， 一般 都 坚持 九大 养身 习惯 ， 你 做 对 几个 ？"
-    for comment, score in server.comment(title):
-        print("%s\t%s"%(comment, score))
-    title = "结婚 婆婆 给 彩礼 18 万 ， 我家 陪嫁 一辆车"
-    for comment, score in server.comment(title):
-        print("%s\t%s"%(comment, score))
+    test_file = sys.argv[1]
+    output_file = sys.argv[2]
+
+    dataset = []
+    with open(test_file, 'r') as fin:
+        for line in fin:
+            dataset.append(line.strip())
+
+    with open(output_file, 'w') as fout:
+        for query in tqdm(dataset):
+            comment_list = getGenerativeCommentList(query)
+            if comment_list:
+                for comment, score in comment_list:
+                    fout.write("%s\t%s\t%s\n"%(query, comment, score))
